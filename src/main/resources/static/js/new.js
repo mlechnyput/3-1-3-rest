51357354c008298d9a5a511eb26cb92822506be0
mlechnyput <mlechnyput1@gmail.com>
2021-01-21 10:34:36 +0300

    не работает

--- src/main/resources/static/js/new.js
+++ src/main/resources/static/js/new.js
@@ -1,4 +1,5 @@
-async function test() {
+// async
+function test() {
 
 let user = {
     firstname: 'John',
@@ -6,20 +7,31 @@ let user = {
     age: 33,
     email: '11111@m.ru',
     password: 'tttrrr',
-    isuser: false,
-    isadmin: true
+    isuser: Boolean(true),
+    isadmin: Boolean(false)
 };
 
-let response = await fetch('/api/new', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user)
-});
+// let response = await fetch('http://localhost:8080/api/new', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json;charset=utf-8'
+//     },
+//     body: JSON.stringify(user)
+// });
+//
+// let result = await response.json();
+// alert(result.message);
 
-let result = await response.json();
-alert(result.message);
+    $.ajax({
+        url: "/api/new",
+        type: "POST",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(user)
+    }).done((msgSave) => {
+        document.location.href='/admin/all'
+
+    })
 }
 
 test()
